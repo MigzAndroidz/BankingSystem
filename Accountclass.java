0375608b75c60dfe2ba7a75c55fd99e638bd18a5
Miguel <118042356+MigzAndroidz@users.noreply.github.com>
2023-05-17 14:54:52 +0800

    Create Accountclass.java

---
+++ Accountclass.java
@@ -0,0 +1,35 @@
+public class Account {
+        private int accountNumber;
+    private String accountHolderName;
+    private double balance;
+
+    public Account(int accountNumber, String accountHolderName, double balance) {
+        this.accountNumber = accountNumber;
+        this.accountHolderName = accountHolderName;
+        this.balance = balance;
+    }
+
+    public int getAccountNumber() {
+        return accountNumber;
+    }
+
+    public String getAccountHolderName() {
+        return accountHolderName;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void deposit(double amount) {
+        balance += amount;
+    }
+
+    public boolean withdraw(double amount) {
+        if (balance >= amount) {
+            balance -= amount;
+            return true;
+        }
+        return false;
+    }
+}
